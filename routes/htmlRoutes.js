1534c3909f1f2682370c92b856ef8be108f2868a
shirano2 <shirano2@naver.com>
2019-04-14 17:07:20 -0500

    finished

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,7 +5,7 @@ var db = require("../models");
 
 /* route for menu screen (after cooking, after eating) */
 router.get("/",function(req,res){
-    db.Burger.findAll({include: [db.Customer]}).then(function(data){
+    db.Burger.findAll({include: [db.Customer],order:[["burger_name","ASC"]]}).then(function(data){
         res.render("index",{burgers:data});
     });
 });
